e7896446446bf570ec4d5c77741e95f7e8852add
lukehalcrow1 <22156@nayland.school.nz>
2025-05-15 15:06:57 +1200

    test gui new method

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("aaaaa")
+import tkinter as tk
+
+from tkinter import messagebox
+
+def button1clicked():
+    messagebox.showinfo("Button 1", "Ydrt")
+def button2clicked():
+    messagebox.showinfo("Button 2", "test")
+
+root = tk.Tk()
+root.title("Testers")
+root.geometry("300x150")
+
+button1 = tk.Button(root, text="Button 1", command=button1clicked)
+button1.pack(pady=10)
+
+button2 = tk.Button(root, text="Button 2", command=button2clicked)
+button2.pack(pady=10)
+
+root.mainloop()
