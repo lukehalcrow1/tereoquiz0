b3cf5e58f65777b2551b1acb78e1bd87d426a7b3
lukehalcrow1 <22156@nayland.school.nz>
2025-05-29 15:14:35 +1200

    added a rolling score and quescount

--- quiz.py
+++ quiz.py
@@ -7,17 +7,17 @@ te_reo_quiz = [
     {"question": "Kai is the Māori word for water.", "answer": False},
     {"question": "Whānau means family.", "answer": True},
     {"question": "Moana is the Māori word for river.", "answer": False},
-    {"question": "Aotearoa is the Māori name for New Zealand.", "answer": True},
+    {"question": "New Zealand is the Māori name for New Zealand.", "answer": False},
     {"question": "Haka is a traditional Māori war dance.", "answer": True},
     {"question": "Waiata means food.", "answer": False},
     {"question": "Ngahere is the Māori word for forest.", "answer": True},
     {"question": "Maunga means mountain.", "answer": True},
     {"question": "Puku means ear.", "answer": False},
     {"question": "Tamariki means children.", "answer": True},
-    {"question": "Waka is the Māori word for kayak.", "answer": True},
-    {"question": "Awa means lake.", "answer": False},
+    {"question": "Waka is the Māori word for waves.", "answer": False},
+    {"question": "Awa means house.", "answer": False},
     {"question": "Whare means house.", "answer": True},
-    {"question": "Whetū means star.", "answer": True},
+    {"question": "Whetū means eight.", "answer": False},
     {"question": "Kura means school.", "answer": True},
     {"question": "Haere mai means go away.", "answer": False},
     {"question": "Kaiako means student.", "answer": False},
@@ -32,14 +32,13 @@ te_reo_quiz = [
     {"question": "Ingoa means name.", "answer": True},
     {"question": "Wāhine means boy.", "answer": False},
     {"question": "Ika means fish.", "answer": True},
-    {"question": "Aroha means love.", "answer": True},
+    {"question": "Aroha means Hate.", "answer": False},
     {"question": "Kōrero means to speak.", "answer": True},
 ]
 
 def add_quescount():
     global quescount
     quescount += 1
-
 quescount = 0 #WHAT question the user is up to
 score = 0 #the users current score
 def playquestion():
@@ -53,26 +52,31 @@ def playquestion():
 
 #belowq here is the actual parts of the quiz that arent functions
 root = tk.Tk()
-root.title("Te Reo Māori quiz")
+root.title("Te Reo quiz")
 root.geometry("800x400")
 
-def trueclicked():
-    add_quescount()
-    if 'answer' == True:
-        print('yo')
+def check(useranswer: bool):
+    global quescount, score
+    correct_answer = te_reo_quiz[quescount]["answer"]
+    if useranswer == correct_answer:
+        score += 1
+    quescount += 1
+    print(score)
+    playquestion()
+
 
 true = tk.Button(root, height=3, width=8,
     text='true',
-    command=trueclicked,
+    command=lambda: check(True),
     bg = 'lawn green'
 )
-true.place(x=250, y=200)
+true.place(relx=0.25, rely=0.5, anchor = "w")
 false = tk.Button(root, height=3, width=8,
     text='false', 
-    command=add_quescount,
+    command=lambda: check(False),
     background ='red'
 )
-false.place(x=500, y=200)
+false.place(relx=0.75, rely=0.5, anchor = "e")
 
 
 
